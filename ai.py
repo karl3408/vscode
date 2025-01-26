74a4b4cc1354eea1eaacd5346a3fa0289c25dd67
karl3408 <karl34408@gmail.com>
2025-01-26 13:33:16 +0900

    ntbook save

--- ai.py
+++ ai.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
 import pandas as pd
 import numpy as np
-
+import requests as rq
+#AIzaSyCMX8pHuzKUlp5AgkBtp3SD12MBhvGuc_g
 class Ai:
     def __init__(self):
-        pass
+        
