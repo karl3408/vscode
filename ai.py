26a13cf3f8f11e16725db885abc4e68855fd0512
karl3408 <karl34408@gmail.com>
2025-03-08 00:11:40 +0900

    sad

--- ai.py
+++ ai.py
@@ -1,9 +1,11 @@
 #supabase password:bDUc47hKFQIr7yz5
+#AIzaSyCMX8pHuzKUlp5AgkBtp3SD12MBhvGuc_g
+#Pg$!7uJ3kX8&zT2#qVwR
 import tensorflow as tf
 import pandas as pd
 import numpy as np
 import requests as rq
-#AIzaSyCMX8pHuzKUlp5AgkBtp3SD12MBhvGuc_g
+
 class Ai:
     def __init__(self):
         pass
