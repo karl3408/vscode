16f61680413a64af8bf1fd3588115be8321ef040
karl3408 <karl34408@gmail.com>
2025-03-17 17:49:50 +0900

    aasdffff

--- ai.py
+++ ai.py
@@ -1,10 +1,6 @@
 #supabase password:bDUc47hKFQIr7yz5
 #AIzaSyCMX8pHuzKUlp5AgkBtp3SD12MBhvGuc_g
 #Pg$!7uJ3kX8&zT2#qVwR
-import tensorflow as tf
-import pandas as pd
-import numpy as np
-import requests as rq
 import pytorch as pt
 import flask as fl
 
