192452f6dbf08c59fc315d2284f9d011cb963e52
karl3408 <karl34408@gmail.com>
2025-03-17 17:51:58 +0900

    df

--- ai.py
+++ ai.py
@@ -1,11 +1,13 @@
 #supabase password:bDUc47hKFQIr7yz5
 #AIzaSyCMX8pHuzKUlp5AgkBtp3SD12MBhvGuc_g
 #Pg$!7uJ3kX8&zT2#qVwR
-import pytorch as pt
+import torch as pt
 import flask as fl
 
 class Ai:
     def __init__(self):
         pass
 
-print(pt.__version__)
+    def print_versions(self):
+        print(pt.__version__)
+        print(fl.__version__)
